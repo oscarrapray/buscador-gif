b921cf26eae57ed9bf89fe33533c0c5019174749
Oscar Rapray <oscar_rm2@yahoo.com>
2020-08-18 20:15:58 -0500

    se trajo datos de la Api

--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const GifCard = ({ title, url }) => {
+const GifCard = ({ title, urlImg }) => {
     return (
         <div className="card">
                 <div className="card-img">
-                <img src={url} alt ={title} />
+                <img src={urlImg} alt ={title} />
                 </div>
                 <div className="card-content">
                    <p>{title}</p>
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,30 +3,29 @@ import axios from 'axios'
 import GifCard from './GifCard'
 
 const GifGrid = ({category}) =>{
-    const[images, setImages] = useState({})
-
+    const[images, setImages] = useState([])
     useEffect(() =>{
-        axios.get(`https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=1tmtPNz0WRpGmNIFLXbj9kLhksv4zfSO`)
+        console.log(category)
+        axios.get(`https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=1tmtPNz0WRpGmNIFLXbj9kLhksv4zfSO`)
         .then(resp => setImages(resp.data))
-    },[])
-    console.log(images)
+    },[])   
     return(
         <div className="card_container">
             {
-                images.length === 0 ?
+                images.length===0 ?
                 <h1>Cargando ...</h1>
                 :
                 (
-                    images.map(image =>(<GifCard 
+                    images.data.map(image =>(
+                        <GifCard 
                         key = {image.id}
-                        id = {image.id}
                         title = {image.title}
-                        url = {image.images.downsized_medium.url}
-                     />))
+                        urlImg = {image.images.downsized_medium.url}
+                     />
+                    ))
                 )
             }
         </div>
     )
 }
-
 export default GifGrid
